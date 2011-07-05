c4588a4dd704a95bac080ee9426988f920a43844
mic <m@micz.it>
2011-07-05 23:22:29 +0200

    first import

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,7 @@
+//pref("extensions.SavedSearchThemAll.boolpref", false);
+//pref("extensions.SavedSearchThemAll.intpref", 0);
+//pref("extensions.SavedSearchThemAll.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.FastCC@micz.it.description", "chrome://FastCC/locale/overlay.properties");
+//pref("extensions.SavedSearchThemAll.AllFromLocalFolders", false);
